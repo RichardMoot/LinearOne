39f0b4a474f5bdfea4b4412092e47d1230bd3f39
Richard Moot <Richard.Moot@labri.fr>
2015-03-08 17:39:57 +0100

    Added type-raised quantifier analysis as alternative to split scope

--- hybrid_grammar.pl
+++ hybrid_grammar.pl
@@ -8,15 +8,21 @@ test(3) :-
 	parse([robin,must,discover,a,solution], s).
 test(4) :-
 	parse([john,cant,eat,steak,and,mary,pizza], s).
+% split scope
 test(5) :-
-	parse([no,neg,fish,walks], s).
+	parse([no2,neg,fish,walks], s).
 test(6) :-
-	parse([no,neg,dog,eats,whiskas,or,neg,cat,alpo], s).
+	parse([no2,neg,dog,eats,whiskas,or2,neg,cat,alpo], s).
+% comparative subdeletion
 test(7) :-
 	parse([john,ate,more,donuts,than,mary,bought,bagels], s).
 test(8) :-
+	parse([no,fish,walks], s).
+test(9) :-
+	parse([no,dog,eats,whiskas,or,cat,alpo], s).
+test(10) :-
 	/* this example makes the need for a better atom selection strategy evident */
-	parse([no,neg,dog,eats,more,whiskas2,than,leslie,buys,donuts,or,neg,cat,alpo2], s).
+	parse([no,dog,eats,more,whiskas2,than,leslie,buys,donuts,or,cat,alpo2], s).
 
 lex(leslie, np, leslie, l).
 lex(robin, np, robin, r).
@@ -52,18 +58,13 @@ lex(to, pp/np, to, lambda(X,X)).
 lex(and, (((s|tv)|(s|tv))|(s|tv)), lambda(STV2,lambda(STV1,lambda(TV,lambda(V,appl(appl(STV1,TV),appl(and,appl(appl(STV2,lambda(W,W)),V))))))), lambda(S2,lambda(S1,lambda(T,bool(appl(S1,T),&,appl(S2,T)))))).
 lex(must, (s|(s|(vp/vp))), lambda(SVP,lambda(Z,appl(appl(SVP,must),Z))), lambda(F,necessary(appl(F,lambda(Y,Y))))).
 lex(cant, (s|(s|(vp/vp))), lambda(SVP,lambda(Z,appl(appl(SVP,cant),Z))), lambda(F,neg(possible(appl(F,lambda(Y,Y)))))).
-lex(no, (s|sneg), lambda(S,lambda(Z,appl(appl(S,no),Z))), neg).
-lex(neg, ((sneg|(s|np))|n), lambda(Phi1,lambda(Sigma,lambda(Phi2,lambda(Z,appl(appl(Sigma,lambda(V,appl(Phi2,appl(neg,appl(Phi1,V))))),Z))))), lambda(X,lambda(Y,quant(exists,Z,bool(appl(X,Z),&,appl(Y,Z)))))).
-% Sigma1/2 (s->s)->(s->s)->(s->s)
-% Phi1/2,or (s->s)
-% T1: Sigma1 Phi1 Phi1: s->s
-% T2: Sigma2 V^V W^W: s->s
-% T1(or (T2 z))
-lex(or, (((sneg|tv)|(sneg|tv))|(sneg|tv)), lambda(Sigma2,lambda(Sigma1,lambda(Phi1,lambda(Phi2,lambda(Z,appl(appl(appl(Sigma1,Phi1),Phi2),appl(or,appl(appl(appl(Sigma2,lambda(V,V)),lambda(W,W)),Z)))))))), lambda(V,lambda(W,lambda(TV,bool(appl(W,TV),\/,appl(V,TV)))))).
+lex(no, (s|(s|h_det)), lambda(Rho,lambda(Z,appl(appl(Rho,lambda(Phi,lambda(Sigma,lambda(V,appl(appl(Sigma,lambda(W,appl(no,appl(Phi,W)))),V))))),Z))), lambda(P,neg(appl(P,lambda(Q,lambda(R,quant(exists,X,bool(appl(Q,X),&,appl(R,X))))))))).
 lex(than, than, than, lambda(X,X)).
-% hdet (s->s)->(s->s->(s->s))->s->s
-% Rho1 ((s->s)->(s->s->(s->s))->s->s)->s->s
-% Phi: s->s
-% Sigma: (s->s)->s->s
-% l
 lex(more, (((s|(s|h_det))|(s|h_det))|than), lambda(Than,lambda(Rho1,lambda(Rho2,lambda(Z,appl(appl(Rho2,lambda(Phi,lambda(Sigma,appl(Sigma,lambda(V,appl(more,appl(Phi,V))))))),appl(Than,appl(appl(Rho1,lambda(Phi2,lambda(Sigma2,lambda(W,appl(appl(Sigma2,Phi2),W))))),Z))))))), lambda(_,lambda(F,lambda(G,bool(number_of(appl(G,lambda(P,lambda(Q,lambda(X,bool(appl(P,X),&,appl(Q,X)))))),gneq,number_of(appl(F,lambda(P2,lambda(Q2,lambda(Y,bool(appl(P2,Y),&,appl(Q2,Y))))))))))))).
+lex(or, ((((s|h_det)|tv)|((s|h_det)|tv))|((s|h_det)|tv)), lambda(Rho2,lambda(Rho1,lambda(Phi,lambda(Tau,lambda(Z,appl(appl(appl(Rho1,Phi),Tau),appl(or,appl(appl(appl(Rho2,lambda(V,V)),lambda(Phi2,lambda(Sigma2,lambda(W,appl(appl(Sigma2,Phi2),W))))),Z)))))))), or).
+
+% lexical entries for "split scope"
+
+lex(no2, (s|sneg), lambda(S,lambda(Z,appl(appl(S,no2),Z))), neg).
+lex(or2, (((sneg|tv)|(sneg|tv))|(sneg|tv)), lambda(Sigma2,lambda(Sigma1,lambda(Phi1,lambda(Phi2,lambda(Z,appl(appl(appl(Sigma1,Phi1),Phi2),appl(or2,appl(appl(appl(Sigma2,lambda(V,V)),lambda(W,W)),Z)))))))), lambda(V,lambda(W,lambda(TV,bool(appl(W,TV),\/,appl(V,TV)))))).
+lex(neg, ((sneg|(s|np))|n), lambda(Phi1,lambda(Sigma,lambda(Phi2,lambda(Z,appl(appl(Sigma,lambda(V,appl(Phi2,appl(neg,appl(Phi1,V))))),Z))))), lambda(X,lambda(Y,quant(exists,Z,bool(appl(X,Z),&,appl(Y,Z)))))).
