13fdb0742a96180f5961a7ece3ce28bf7a4f368c
Richard Moot <Richard.Moot@labri.fr>
2015-03-21 22:28:34 +0100

    Added progress indicator

--- mill1.pl
+++ mill1.pl
@@ -112,7 +112,7 @@ parse(ListOfWords, Goal0) :-
 	N is N0 + 1,
 	retractall('$LOOKUP'(_)),
 	assert('$LOOKUP'(N)),
-        format(user_error, '~N= Lookup ~w~n', [N]),
+        format(user_error, '~N= Lookup ~D~n', [N]),
 	prove0(Formulas, Goal, LexSem),
 	fail.
 parse(_, _) :-
@@ -182,6 +182,7 @@ prove0(Antecedent, Goal) :-
 prove0(Antecedent, Goal, LexSem) :-
         unfold_sequent(Antecedent, Goal, Roots, Graph, Sem0, Stats),
 	portray_sequent_statistics(Stats),
+	format(user_error, 'Parsing: ', []),
 	/* keep a copy of the initial graph (before any unificiations) for later proof generation */
 	copy_term(Graph, GraphCopy),
 	portray_graph(Graph),
@@ -246,6 +247,7 @@ prove1(Graph0, Roots0, [ax(N0,AtV0,AtO0,N1,AtV1,AtO1)|Rest0]) :-
 	contract(Graph4, Graph, Rest0, Rest, Roots1, Roots),
 	/* verify the result is (at least potentially) connected */
 	connected(Graph),
+	mark_progress(Graph),
 	prove1(Graph, Roots, Rest).
 
 % =======================================
@@ -300,6 +302,10 @@ contract1(Graph0, [vertex(N1,Cs,FVs,Rs)|Graph], [N0-univ(U,N1)|Rest], Rest, Root
 %
 % verify that the module above the forall link has no remaining occurrences of the eigenvariable
 % of the link in atomic formulas
+%
+% TODO: this can surely be done in a smarter way. For example, it only makes sense to try and
+% contract a forall link after all atomic formulas containing its eigenvariable have been
+% connected.
 
 no_occurrences([], Ps, G, V) :-
 	no_occurrences_pars(Ps, G, V).
@@ -334,7 +340,7 @@ no_occurrences_par(par(N1,N2), V, G0, G) :-
     (
 	selectchk(vertex(N1,Cs,_,Rs), G0, G1)
     ->			 
-	no_occurrences(Cs, Rs, G, V)
+	no_occurrences(Cs, Rs, G1, V)
     ;
         G1 = G0
     ),
@@ -568,6 +574,12 @@ number_subformulas_pos(impl(A,B), C, N0, N, M, M, C-impl(NA,NB)) :-
 % =======================================
 % =             Input/output            =
 % =======================================
+
+mark_progress(G) :-
+	length(G, L),
+	format(user_error, '~D.', [L]),
+        flush_output(user_error).
+	
 
 % = sequent statistics predicates
 
