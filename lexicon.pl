f5b64101c7e10493ec16f0364b85453c10971457
Richard Moot <richard.moot@icloud.com>
2020-11-12 18:46:54 +0100

    Update lexicon.pl

--- lexicon.pl
+++ lexicon.pl
@@ -1,7 +1,16 @@
-:- module(lexicon, [lookup/4, lookup/5, macro_expand/2, lexical_lookup/5, lex_to_displacement/2, lex_to_hybrid/3,canonical_semantic_term/2]).
+% -*- Mode: Prolog -*-
+
+:- module(lexicon, [lookup/4,
+		    lookup/5,
+		    macro_expand/2,
+		    lexical_lookup/5,
+		    lex_to_displacement/2,
+		    lex_to_hybrid/3,
+		    canonical_semantic_term/2]).
 
 :- use_module(translations, [translate/3, translate_hybrid/6,linear_to_hybrid/3,linear_to_displacement/3]).
 :- use_module(auxiliaries, [universal_closure/2,universal_disclosure/2, is_ll1_formula/2]).
+%:- use_module(prenex, [prenex/2,pushin/2]).
 
 % define operators to allow for easier specification of
 % hybrid and displacement lexical entries.
@@ -9,7 +18,7 @@
 % WARNING: in case of doubt, use parentheses to disambiguate!
 % I have deliberately not changed the definitions of standard
 % mathematical and logical operations of Prolog, notably |
-% (alternative of ; for use in DCG), \ and *.
+% (alternative of ; for use in DCG), / and *.
 %
 % This means for example that:
 % c/d*b/c = ((c/d)*b)/c
@@ -30,6 +39,13 @@
 
 :- dynamic hybrid_lookup/3, memo_lookup/2.
 
+update(F0, F) :-
+	replace_s_levels(F0, F).
+%	F0 = F.
+%	pushin(F0, F).
+%	prenex(F0, F).
+
+
 lookup(Words, Formulas, Goal, ExpandedGoal) :-
 	lookup(Words, Formulas, _, Goal, ExpandedGoal).
 
@@ -37,7 +53,8 @@ lookup(Words, Formulas, Semantics, Goal0, ExpandedGoal) :-
 	retractall(hybrid_lookup(_, _, _)),
 	sentence_lookup(Words, Formulas, Semantics, 0, N),
 	macro_expand(Goal0, Goal),
-	translate(Goal, [0, N], ExpandedGoal).
+	translate(Goal, [0, N], ExpandedGoal0),
+	update(ExpandedGoal0, ExpandedGoal).
 
 sentence_lookup([], [], [], N, N).
 sentence_lookup([W|Ws], [F|Fs], [N0-S|Ss], N0, N) :-
@@ -54,7 +71,8 @@ lexical_lookup(Word, Formula, Semantics, N0, N1) :-
 	lex(Word, Formula0, Semantics0),
 	canonical_semantic_term(Semantics0, Semantics),    
 	macro_expand(Formula0, Formula1),
-        translate(Formula1, [N0,N1], Formula),
+        translate(Formula1, [N0,N1], Formula2),
+	update(Formula2, Formula),
         retractall(memo_lookup(N0, _)),
         assert(memo_lookup(N0, Word))
     ;
@@ -67,7 +85,8 @@ lexical_lookup(Word, Formula, Semantics, N0, N1) :-
 	copy_term(ProsTerm0, ProsTerm),
 	canonical_semantic_term(Semantics0, Semantics),    
 	macro_expand(Formula0, Formula1),
-        translate_hybrid(Formula1, ProsTerm, Word, N0, N1, Formula),
+        translate_hybrid(Formula1, ProsTerm, Word, N0, N1, Formula2),
+	update(Formula2, Formula),
 	retractall(hybrid_lookup(N0, _, _)),
 	assert(hybrid_lookup(N0, Formula1, ProsTerm))
     ;
@@ -76,8 +95,9 @@ lexical_lookup(Word, Formula, Semantics, N0, N1) :-
         lex(Word, _, _, _, _)
     ->
 	lex(Word, Formula0, N0, N1, Semantics0),
-	macro_expand(Formula0, Formula),
-	is_ll1_formula(Formula, Word),
+	macro_expand(Formula0, Formula1),
+	is_ll1_formula(Formula1, Word),
+	update(Formula1, Formula),
 	/* prevent potential errors caused by accidental sharing of variables between ProsTerm and Semantics0 */
 	canonical_semantic_term(Semantics0, Semantics)
     ;
@@ -115,6 +135,10 @@ macro_expand(h_det, F) :-
 macro_expand(h_det_c, F) :-
 	!,
 	macro_expand(((s|(s|np(_)))|n), F).
+macro_expand(l_q_sub, dr(at(s),dl(at(np),at(s)))) :-
+	!.
+macro_expand(l_det_sub, dr(dr(at(s),dl(at(np),at(s))),at(n))) :-
+	!.
 macro_expand(tv, dr(dl(at(np),at(s)),at(np))) :-
 	!.
 macro_expand(vp, dl(at(np),at(s))) :-
@@ -123,6 +147,18 @@ macro_expand(tv_c, dr(dl(at(np, [nom]),at(s)),at(np, [acc]))) :-
 	!.
 macro_expand(vp_c, dl(at(np, [nom]),at(s))) :-
 	!.
+macro_expand(tv_q, dr(dl(at(np),at(s1)),at(np))) :-
+	!.
+macro_expand(vp_q, dl(at(np),at(s1))) :-
+	!.
+macro_expand(lq12, dr(at(s1),dl(at(np),at(s2)))) :-
+	!.
+macro_expand(hq12, h(at(s1),h(at(s2),at(np)))) :-
+	!.
+macro_expand(hdet12, h(h(at(s1),h(at(s2),at(np))),at(n))) :-
+    !.
+macro_expand(ldet12, dr(dr(at(s1),dl(at(np),at(s2))),at(n))) :-
+    !.
 macro_expand(F, Formula) :-
 	current_predicate(user:atomic_formula/3),
 	user:atomic_formula(F, At, Vars),
@@ -296,3 +332,23 @@ canonical_semantics_list([], []).
 canonical_semantics_list([X|Xs], [Y|Ys]) :-
 	canonical_semantics(X, Y),
 	canonical_semantics_list(Xs, Ys).
+
+replace_s_levels(at(s1, Ys), forall(X,at(s, [X|Ys]))) :-
+	!.
+replace_s_levels(at(s2, Ys), forall(X,at(s, [s(X)|Ys]))) :-
+	!.
+replace_s_levels(at(s3, Ys), forall(X,at(s, [s(s(X))|Ys]))) :-
+	!.
+replace_s_levels(at(s4, Ys), forall(X,at(s, [s(s(s(X)))|Ys]))) :-
+	!.
+replace_s_levels(at(X,Ys), at(X,Ys)).
+replace_s_levels(p(A0,B0), p(A,B)) :-
+	replace_s_levels(A0, A),
+	replace_s_levels(B0, B).
+replace_s_levels(impl(A0,B0), impl(A,B)) :-
+	replace_s_levels(A0, A),
+	replace_s_levels(B0, B).
+replace_s_levels(forall(X,A0), forall(X,A)) :-
+	replace_s_levels(A0, A).
+replace_s_levels(exists(X,A0), exists(X,A)) :-
+	replace_s_levels(A0, A).
